b4e0ca39c6b445ffdde1fc1a77f5853fbebc4044
Muhammad Umer Tariq <39294312+umertariq22@users.noreply.github.com>
2025-07-17 02:17:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Responsive navbar toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+if (navToggle && navMenu) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+}
